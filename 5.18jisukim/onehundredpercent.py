4a344ad96c44ad53fd679691187ead1e81c41bc8
jisu <onpaso53@gmail.com>
2023-05-18 11:22:32 +0900

    solve 백발백중 another way

--- 5.18jisukim/onehundredpercent.py
+++ 5.18jisukim/onehundredpercent.py
@@ -1,14 +1,12 @@
 # 백발백중
-# import math => math.sqrt
+# import math => math.sqrt로도 써도 된다.
 import sys
 input = sys.stdin.readline
 
 x1, y1, r1 = map(int, input().split())
 x2, y2, r2 = map(int, input().split())
 
-distance = ((x1-x2)**2 + (y1-y2)**2)
-
-if distance >= (r1+r2)**2:
+if ((x1-x2)**2 + (y1-y2)**2) >= (r1+r2)**2:
     print("NO")
 else:
     print("YES")
---
+++ app.py
@@ -0,0 +1,54 @@
+# 머신러닝 특강 내용
+# https://docs.streamlit.io/library/api-reference
+# https://news.hada.io/
+# pip install streamlit openai
+
+import streamlit as st
+import openai
+
+# st.write("Hello World!")
+# streamlit run app.py 터미널에서 실행하기
+
+openai.api_key = "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"
+
+st.balloons()
+
+st.title("ChatGPT Plus DALL-E!")
+
+with st.form("form"):
+    user_input = st.text_input("Prompt")
+    submit = st.form_submit_button("Submit")
+
+
+if submit:
+    # st.write(user_input)
+
+    gpt_prompt = []
+
+    gpt_prompt.append({
+        "role": "system",
+        "content": "Imagine the detail appearance of the input. Response shortly. Translate to english"
+    })
+
+    gpt_prompt.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    with st.spinner("Waiting for chatGPT..."):
+        prompt = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=gpt_prompt)
+
+    # st.markdown(prompt["choices"][0]["message"]["content"])
+
+    prompt = prompt["choices"][0]["message"]["content"]
+    st.caption(prompt)
+
+    # DALL-E
+    with st.spinner("Waiting for DALL-E..."):
+        result = openai.Image.create(
+            prompt=prompt,
+            size="1024x1024"
+        )
+
+    st.image(result["data"][0]["url"])
