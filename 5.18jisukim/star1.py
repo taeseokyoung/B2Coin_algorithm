ba8f28ae80786a677711ab6224e1c853b83d5071
jisu <onpaso53@gmail.com>
2023-05-19 10:47:04 +0900

    ✨ Feature: 5.18 algorithm

--- 5.18jisukim/star1.py
+++ 5.18jisukim/star1.py
@@ -4,3 +4,12 @@ import sys
 input = sys.stdin.readline
 for i in range(int(input())):
     print('*'*(i+1))
+
+# 별찍기 재귀
+def solution(n):
+    if n <= 1:
+        return n
+    print("*" * solution(n - 1))
+    return n
+
+solution(int(input())+1)
