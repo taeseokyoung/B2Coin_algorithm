463bd77dc699721b42f34d6f944987af9f4811b2
jisu <onpaso53@gmail.com>
2023-05-19 10:38:18 +0900

    ✨ Feat: 5.19 algorithm

--- 5.19jisukim/star2.py
+++ 5.19jisukim/star2.py
@@ -1,9 +1,9 @@
 # 백준 별찍기2
-
-# sol1 그냥 풀어보기
 import sys
 input = sys.stdin.readline
 n = int(input())
+
+# sol1 그냥 풀어보기
 for i in range(1,n+1):
     print(' '*(n-i)+'*'*i)
 
@@ -13,4 +13,5 @@ for i in range(n):
     print(star.rjust(n))
     # 전체 n에서 오른쪽 정렬을 하는 .rjust(n) 사용
 
-# 재귀
+# sol3 재귀로 풀기
+
